df64637c0aa20780c8f5b9d43db44db5f02c074f
Felice Forby <feliceforby@gmail.com>
2019-03-19 23:20:54 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,6 +6,10 @@ const NYT_API_KEY = 'Iu5OvsSMnrfFVzTJ8aHzsqIl2GO8GLgg';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
+<<<<<<< HEAD
+=======
+// Code LatestMovieReviewsContainer Here
+>>>>>>> cc7df9ab9c1e49d4a2c57e4563d05333f806a5c8
 class LatestMovieReviewsContainer extends Component {
   constructor() {
     super();
@@ -15,6 +19,7 @@ class LatestMovieReviewsContainer extends Component {
     }
   }
   
+<<<<<<< HEAD
   componentWillMount() {
     fetch(URL)
       .then(response => response.json())
@@ -25,6 +30,12 @@ class LatestMovieReviewsContainer extends Component {
     return (
       <div className="latest-movie-reviews">
         <MovieReviews reviews={this.state.reviews} />
+=======
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+      
+>>>>>>> cc7df9ab9c1e49d4a2c57e4563d05333f806a5c8
       </div>
     )
   }
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,6 +6,10 @@ const NYT_API_KEY = 'Iu5OvsSMnrfFVzTJ8aHzsqIl2GO8GLgg';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
+<<<<<<< HEAD
+=======
+// Code SearchableMovieReviewsContainer Here
+>>>>>>> cc7df9ab9c1e49d4a2c57e4563d05333f806a5c8
 class SearchableMovieReviewsContainer extends Component {
   constructor() {
     super();
@@ -16,6 +20,7 @@ class SearchableMovieReviewsContainer extends Component {
     }
   }
   
+<<<<<<< HEAD
   handleSubmit = event => {
     event.preventDefault();
     const query = this.state.searchTerm;
@@ -45,6 +50,11 @@ class SearchableMovieReviewsContainer extends Component {
           <h2>Review Search Results</h2>
         }
         <MovieReviews reviews={this.state.reviews} />
+=======
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+>>>>>>> cc7df9ab9c1e49d4a2c57e4563d05333f806a5c8
       </div>
     )
   }
