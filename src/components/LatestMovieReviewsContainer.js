889f4357ab5e783d1362088d22392ed1d22a6736
Felice Forby <feliceforby@gmail.com>
2019-03-19 23:22:54 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,10 +6,6 @@ const NYT_API_KEY = 'Iu5OvsSMnrfFVzTJ8aHzsqIl2GO8GLgg';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-<<<<<<< HEAD
-=======
-// Code LatestMovieReviewsContainer Here
->>>>>>> cc7df9ab9c1e49d4a2c57e4563d05333f806a5c8
 class LatestMovieReviewsContainer extends Component {
   constructor() {
     super();
@@ -19,7 +15,6 @@ class LatestMovieReviewsContainer extends Component {
     }
   }
   
-<<<<<<< HEAD
   componentWillMount() {
     fetch(URL)
       .then(response => response.json())
