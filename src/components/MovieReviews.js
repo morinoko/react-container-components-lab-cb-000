a4409af886540f9adb8c363812363e0563d10964
Felice Forby <feliceforby@gmail.com>
2019-03-19 02:02:01 +0000

    initial component setup

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,8 @@
 // Code MovieReviews Here
+const MovieReview = (props) => {
+  <ul className="review-list">
+  
+  </ul>
+};
+
+export default MovieReview;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,22 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+  
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer;
